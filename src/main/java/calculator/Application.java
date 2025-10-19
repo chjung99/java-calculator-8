cf1cb800fe633a2550063691d3251251ed9930f5
chjung99 <cksgh1168@gmail.com>
2025-10-19 16:30:49 +0900

    feat(application): 메인 프로그램 실행 흐름 구현
    - 모든 컴포넌트를 연결하여 전체 프로그램 흐름 완성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,20 @@
 package calculator;
 
+import calculator.core.Calculator;
+import calculator.input.InputReader;
+import calculator.output.MessagePrinter;
+import calculator.parser.InputParser;
+
+import java.util.ArrayList;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        MessagePrinter.printInputMessage();
+        InputReader inputReader = new InputReader(readLine());
+        ArrayList<Integer> parsedArrayList = InputParser.parse(inputReader.getInputString());
+        int result = Calculator.calculateSum(parsedArrayList);
+        MessagePrinter.printOutputMessage(result);
     }
 }
